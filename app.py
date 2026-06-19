ba4f00483c9d43d8b756d1e069d6af6cc111d70c
Madankbr <madankbr1603@gmail.com>
2026-06-20 00:04:40 +0530

    Add /submittodoitem backend route with MangoDb storage

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, jsonify, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+# MongoDB connection
+client = MongoClient("mongodb://localhost:27017/")
+db = client["todo_db"]
+todo_collection = db["todo_items"]
 
 @app.route('/')
 def home():
@@ -13,6 +19,23 @@ def api():
         "course": "DevOps Assignment",
         "status": "active"
     })
+
+@app.route('/submittodoitem', methods=['POST'])
+def submit_todo_item():
+    data = request.get_json()
+    item_name = data.get('itemName')
+    item_description = data.get('itemDescription')
+
+    todo_collection.insert_one({
+        "itemName": item_name,
+        "itemDescription": item_description
+    })
+
+    return jsonify({
+        "message": "Item added successfully",
+        "itemName": item_name,
+        "itemDescription": item_description
+    }), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
