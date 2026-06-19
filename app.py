4c1d0df2700b85dee183be1254eaba11908ab153
Madankbr <madankbr1603@gmail.com>
2026-06-19 23:59:44 +0530

    Add To-Do page with Item Name and Item Description fields

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
@@ -13,6 +13,10 @@ def api():
         "course": "DevOps Assignment",
         "status": "active"
     })
+
+@app.route('/todo')
+def todo():
+    return render_template('todo.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
